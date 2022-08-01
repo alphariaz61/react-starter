60303bb9f1862f0f63e94e69f0dd812d28945d03
shaum316 <shaum316@gmail.com>
2022-08-01 09:38:21 -0700

    Second Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App () {
+    return (
+        <div className="wrapper bg-dark text-white">
+            <div className="container pt-5">
+                <h1 className="text-center">Hello React World</h1>
+            </div>
+        </div>
+    ) 
+}
